45177395e4e97b311584f8772418a848442aa00e
Valentina Ovsianik <forovp@gmail.com>
2024-12-04 23:07:05 +0300

    Установлена библиотека Redis и добавлены настройки для кэширования

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "mailing:index"
 LOGOUT_REDIRECT_URL = "users:login"
+
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
+    }
