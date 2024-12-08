e361ae8c4e777b663fd960e517bb7fe0b1edb392
Valentina Ovsianik <forovp@gmail.com>
2024-12-08 18:17:45 +0300

    Добавлено отображение статуса is_active

--- mailing/admin.py
+++ mailing/admin.py
@@ -5,3 +5,7 @@ from .models import Mailing, Message, Recipient
 admin.site.register(Recipient)
 admin.site.register(Message)
 admin.site.register(Mailing)
+
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("message", "status", "is_active")
+    list_editable = ("is_active",)
